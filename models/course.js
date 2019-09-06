09b790b9286757ea9ed3477e835df243f3f12fa3
Lujanhi <51241524+Lujanhi@users.noreply.github.com>
2019-09-06 10:03:10 -0400

    save 4

---
+++ models/course.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const Course = sequelize.define('Course', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        title: DataTypes.STRING,
+        description: DataTypes.TEXT,
+
+        estimatedTime: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+
+        materialsNeeded: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+
+
+    }, {});
+
+    //Relationship between the Courses and Users tables is defined (linking tables)
+    Course.associate = function (models) {
+        Course.belongsTo(models.User, {
+            as: 'user',
+            foreignKey: {
+                fieldName: 'userId',
+                allowNull: false,
+            },
+        }); //auto-generate userId in Course linking to User model
+    };
+    return Course;
+};
---
+++ models/user.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        firstName: DataTypes.STRING,
+        lastName: DataTypes.STRING,
+        emailAddress: DataTypes.STRING,
+        password: DataTypes.STRING
+    }, {});
+    //Relationship between the Users and Courses tables is defined (linking tables)
+    User.associate = function (models) {
+        User.hasMany(models.Course); //grants access to user.getCourses()
+    };
+    return User;
+};
